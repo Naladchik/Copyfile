8fef3845cc085fbcb415a744487f440a64a8ebe3
Alex Viaselyeu <alexey.veseliev@gmail.com>
2025-08-22 11:19:38 +0200

    Updated for fast execution without entering file names

--- threads.cpp
+++ threads.cpp
@@ -31,7 +31,7 @@ void readFile(const std::string& sourceFileName) {
         {
             std::unique_lock<std::mutex> lock(mtx);
             cv.wait(lock, [] { return !hasData; });
-            buffer.assign(tempBuffer, tempBuffer + bytesRead);
+            buffer.assign(tempBuffer, tempBuffer + bytesRead); // [action] copiing must be optimized
             hasData = true;
         }
         cv.notify_one(); // Notify the writer thread that data is available
@@ -73,11 +73,14 @@ int main() {
     std::string sourceFileName, destinationFileName;
 
     // Get the source and destination file names
-    std::cout << "Enter the source file name: ";
-    std::cin >> sourceFileName;
-    std::cout << "Enter the destination file name: ";
-    std::cin >> destinationFileName;
+    //std::cout << "Enter the source file name: ";
+    //std::cin >> sourceFileName;
+    //std::cout << "Enter the destination file name: ";
+    //std::cin >> destinationFileName;
+    sourceFileName = "file_to_copy.txt";
+    destinationFileName = "copy.txt";
 
+    std::cout << "copying started" << std::endl;
     // Create the reader and writer threads
     std::thread readerThread(readFile, sourceFileName);
     std::thread writerThread(writeFile, destinationFileName);
