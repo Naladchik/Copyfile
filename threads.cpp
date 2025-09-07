728cb5204dae90c507b826737e9164e9906279a9
Alex Viaselyeu <alexey.veseliev@gmail.com>
2025-09-07 16:28:42 +0200

    Update threads.cpp

--- threads.cpp
+++ threads.cpp
@@ -7,52 +7,53 @@
 #include <filesystem>
 #include <chrono>
 
+using namespace std;
+
 #define CHUNK_SIZE 65535
-#define ONE 1
-#define TWO 2
 
 // sharable global variables
 char buf1[CHUNK_SIZE];
 char buf2[CHUNK_SIZE];
-char p_c_end = 0;  // write in producer read in consumer
-int  p_c_actual_size = 0;
-bool p_c_b1_full = false;  // write in producer read in consumer
-bool p_c_b2_full = false;  // write in producer read in consumer
-bool c_p_b1_empty = true;  // write in consumer read in producer
-bool c_p_b2_empty = true;  // write in consumer read in producer
+char * buf_from_file = buf1;
+char * buf_to_file = buf1;
+unsigned char p_c_actual_size;
+bool data_ready = false;
+bool write_finished = true;
+bool finish = false;
+
+mutex mtx;
+condition_variable cv1, cv2;
+
+void buff_toggle(char ** buf){
+    if(*buf == buf1) *buf = buf2; else *buf = buf1;
+}
 
 
 void producer(const std::string& input_file_name)
 {
     // ===== intro part =====
     std::ifstream input_file(input_file_name, std::ios::binary);
-    char p_buf_in_use = ONE;  // can be ONLY 1 or 2
 
     // ===== loop ===========
     while (!input_file.eof()) {
-        if(ONE == p_buf_in_use){
-            while(! c_p_b1_empty){}  // wait for signal from consumer
-            c_p_b1_empty = false;
-
-            input_file.read(buf1, CHUNK_SIZE);
-            p_c_actual_size = input_file.gcount();  // transmit size to consumer
-
-            p_c_b1_full = true;  // send signal to cunsumer
-            // p_buf_in_use = TWO;  // internal switch
-        }else{  // TWO == p_buf_in_use
-            while(! c_p_b2_empty){}  // wait for signal from cunsumer
-            c_p_b2_empty = false;
-
-            input_file.read(buf2, CHUNK_SIZE);
-            p_c_actual_size = input_file.gcount();  // transmit size to consumer
-
-            p_c_b2_full = true;  // send signal to cunsumer
-            p_buf_in_use = ONE;  // internal switch
+        {
+            unique_lock<mutex> lock(mtx);
+            cv2.wait(lock, [] { return write_finished; });
+            write_finished = false; 
         }
+        cout << "reading" << endl;
+        input_file.read(buf_from_file, CHUNK_SIZE);
+        p_c_actual_size = input_file.gcount();  // transmit size to consumer
+        {
+            lock_guard<mutex> lock(mtx);
+            data_ready = true;        
+            cv1.notify_one();
+        }
+        buff_toggle(&buf_from_file);
     }
 
     // ===== final part =====
-    p_c_end = 1;
+    finish = true;
     input_file.close();
 }
 
@@ -60,29 +61,23 @@ void consumer(const std::string& output_file_name)
 {
     // ===== intro part =====
     std::ofstream output_file(output_file_name, std::ios::binary);
-    char c_buf_in_use = ONE;  // can be ONLY 1 or 2
 
     // ===== loop ===========
     while(true){
-        if(ONE == c_buf_in_use){
-            while(! p_c_b1_full){}  // wait for signal from producer
-            p_c_b1_full = false;
-
-            output_file.write(buf1, p_c_actual_size);
-
-            c_p_b1_empty = true;  // send signal to producer
-            // c_buf_in_use = TWO;  // internal switch
-        }else{  // TWO == c_buf_in_use
-            while(! p_c_b2_full){}  // wait for signal from producer
-            p_c_b2_full = false;
-
-            output_file.write(buf2, p_c_actual_size);
-
-            c_p_b2_empty = true;  // send signal to producer
-            c_buf_in_use = ONE;  // internal switch
+        {
+            unique_lock<mutex> lock(mtx);
+            cv1.wait(lock, [] { return data_ready; });
+            data_ready = false; 
         }
-
-        if(p_c_end)break;
+        cout << "writing" << endl;
+        output_file.write(buf_to_file, p_c_actual_size);
+        buff_toggle(&buf_to_file);
+        {
+            lock_guard<mutex> lock(mtx);
+            write_finished = true;        
+            cv2.notify_one();
+        }        
+        if(finish) break;
     }
 
     // ===== final part =====    
